3d470db29e30692cfd59962a366963594809400d
smcgee31 <solodesign31@gmail.com>
2018-01-31 22:51:44 -0700

    fix: puts routes in their own file

--- server/app.js
+++ server/app.js
@@ -6,28 +6,16 @@ const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const bodyParser = require('body-parser');
 const passport = require('./services/passport');
+const routes = require('./routes');
+const errorHandlers = require('./handlers/errorHandlers');
 
-// CONTROLLERS //
-const UserCtrl = require('./controllers/UserCtrl');
-const ShiftCtrl = require('./controllers/ShiftCtrl');
-
-// POLICIES //
-const isAuthed = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.status(401).send();
-  }
-
-  return next();
-};
-
-// EXPRESS //
 const app = express();
 
 app.use(express.static(`${ __dirname }./../public`));
 // app.use(favicon(__dirname + './../public/img/favicon.ico'));
 app.use(bodyParser.json());
 
-// Session and passport
+// Session and Passport
 app.use(session({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
@@ -36,8 +24,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Passport Endpoints
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/me',
 }));
@@ -47,15 +33,16 @@ app.get('/logout', function(req, res) {
   return res.status(200).send('logged out');
 });
 
-// User Endpoints
-app.post('/register', UserCtrl.register);
-app.get('/user', UserCtrl.read);
-app.get('/me', isAuthed, UserCtrl.me);
-app.put('/user/:_id', isAuthed, UserCtrl.update);
+app.use('/', routes);
 
-// Other Endpoints
-app.post('/newShift/:id', ShiftCtrl.createNewShift);
-app.put('/addTrips/:id', ShiftCtrl.createTrip);
-app.get('/addTrips/getShifts/:id', ShiftCtrl.getUsersShifts);
+// Handle Errors
+//  -are they just validation errors
+app.use(errorHandlers.flashValidationErrors);
+//  -or are they developer errors
+if (app.get('env') === 'development') {
+  app.use(errorHandlers.developmentErrors);
+}
+//  -if none of the above then they are production errors
+app.use(errorHandlers.productionErrors);
 
 module.exports = app;
